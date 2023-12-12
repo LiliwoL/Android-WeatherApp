8b0b62b156440470d80b8d634e2be2d9ad6ac994
LiliwoL <github@liliwol.fr>
2023-12-12 17:00:10 +0100

    Up

--- app/src/main/java/com/liliwol/weatherapp/location/LocationCord.java
+++ app/src/main/java/com/liliwol/weatherapp/location/LocationCord.java
@@ -4,6 +4,5 @@ public class LocationCord {
     public static String lat = "";
     public static String lon = "";
         public final static String API_KEY = "ca18014071190091d4be752b98e34330";
-//    public final static String API_KEY = "eeb8b40367eee691683e5a079e2fa695";
 }
 
